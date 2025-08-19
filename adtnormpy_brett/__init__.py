eff1eb632a475e67d0092555c5e4d6eaa2120648
Brett Ransegnola <67807850+bpr4242@users.noreply.github.com>
2025-08-19 12:58:10 -0400

    Update __init__.py

--- adtnormpy_brett/__init__.py
+++ adtnormpy_brett/__init__.py
@@ -1,1 +1,1 @@
-from .ADTnormPy import adtnorm, install_adtnorm_R, landmarks_to_r, landmarks_to_python, save_python_landmarks, load_python_landmarks, run_marker_parallel
+from .ADTnormPy import adtnorm, install_adtnorm_R, landmarks_to_r, landmarks_to_python, save_python_landmarks, load_python_landmarks, adtnorm_parallel_markers
